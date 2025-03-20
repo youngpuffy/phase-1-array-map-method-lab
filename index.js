a7db6202bf83ceac7114117442f9a04529ca6af1
Youngpuffy <levimuturi2@gmail.com>
2025-03-20 15:14:07 +0300

    map method

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(str =>
+    str
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() +word.slice(1))
+    .join(" ")
+  
+  );
 }
